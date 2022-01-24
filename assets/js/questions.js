d6f57b593f7e3e4ad513f02b062969a630de2091
A. Dorsey <ashadorsey@gmail.com>
2022-01-23 20:43:06 -0500

    created questions in js for quiz

---
+++ assets/js/questions.js
@@ -0,0 +1,75 @@
+// 10 CODE QUIZ questions, choices, and answers
+var questions = [
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["Strings", "Booleans", "Alerts", "Numbers"],
+      answer: "Alerts"
+    },
+    {
+      title: "Which one is a looping structure in JavaScript?",
+      choices: ["For", "While", "do-while loops", "All the above"],
+      answer: "All the above"
+      },
+    {
+      title:"String values must be enclosed within ____ when being assigned to variables.",
+      choices: ["commas", "curly brackets", "quotes", "parentheses"],
+      answer: "quotes"
+      },
+    {
+      title: "Arrays in JavaScript can be used to store ____.",
+      choices: [
+        "numbers and strings",
+        "other arrays",
+        "booleans",
+        "all of the above"
+    ],
+      answer: "all of the above"
+      },
+    {
+      title:"A very useful tool used during development and debugging for printing content to the debugger is:",
+      choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+      answer: "console.log"
+      },
+ 
+    {
+      title: "What is the type of Pop up boxes available in JavaScript?:",
+      choices: ["Alert", "Confirm", "Prompt", "All the above"],
+      answer: "All the above"
+      },
+   
+    {
+      title: "Which IF statement is correct if 'i' is NOT equal to 10?",
+      choices: [
+        "if (i !=== 10)",
+        "if (i <> 10)",
+        "if i =! 10",
+        "if (i != 10)"
+      ],
+      answer: "if (i != 10)"
+    },
+    {
+      title: "Boolean operators that can be used in JavaScript include:",
+      choices: [
+        "'And' Operator &&",
+        "'Or' Operator ||",
+        "'Not' Operator !",
+        "All the above"
+      ],
+      answer: "All the above"
+    },
+    {
+      title: "What is the data type of variables in JavaScript?",
+      choices: [
+        "Object data types",
+        "Function data type",
+        "None of the above",
+        "All of the above"
+      ],
+      answer: "Object data types"
+    },
+  ];
