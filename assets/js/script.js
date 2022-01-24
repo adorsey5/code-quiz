980e522d2952f844fa01244b27df85e1d8755ec8
A. Dorsey <ashadorsey@gmail.com>
2022-01-23 20:50:20 -0500

    created the beginning of the quiz and timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,19 @@ var remarkEl = document.querySelector("#remark");
 var time = questions.length * 15; 
 var timerId;
 var currentQuestionIndex = 0;
+
+// START QUIZ & TIMER
+function startQuiz() {
+    // HIDE START PAGE
+    var startScreenEl = document.getElementById("start-page"); 
+    startScreenEl.setAttribute("class", "hide"); 
+  
+     // DISPLAY TIME AT START OF QUIZ
+     timerEl.textContent = time;
+     // BEGIN TIMER
+     timerId = setInterval(secondTick, 1000);
+  
+    // SHOW QUESTION PAGE
+    questionsEl.removeAttribute("class"); 
+    showQuestion();
+  }
